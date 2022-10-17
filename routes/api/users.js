8910378579cf528691d0cd1902092f4919041799
Moskalenko Serhii <mynameserhii@gmail.com>
2022-10-17 17:41:05 +0300

    50%

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,13 +2,22 @@ const express = require('express');
 const auth = require('../../middlewares/auth');
 const upload = require('../../service/upload');
 const { usersPostSchema, usersSubscSchema } = require('../../validate');
-const { signup, login, logout, current, subscription, avatars } = require('../../controller');
+const {
+  signup,
+  login,
+  logout,
+  current,
+  subscription,
+  avatars,
+  verify,
+} = require('../../controller');
 
 const router = express.Router();
 
 router.post('/signup', usersPostSchema, signup);
 router.post('/login', usersPostSchema, login);
 router.get('/logout', auth, logout);
+router.get('/verify/:verificationToken', verify);
 router.get('/current', auth, current);
 router.patch('/', auth, usersSubscSchema, subscription);
 router.patch('/avatars', auth, upload.single('avatars'), avatars);
